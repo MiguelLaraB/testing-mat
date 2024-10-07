15b8c6b000cbf5f05a8174c30c3aac71cdedd206
MiguelLaraB <zS22004395@estudiantes.uv.mx>
2024-10-07 17:25:18 -0600

    add greet branch #3

--- src/app/factorial/factorial.ts
+++ src/app/factorial/factorial.ts
@@ -1,32 +1,32 @@
-export function factorial (x: number){
+    export function factorial (x: number){
 
-    if(x<0){
+        if(x<0){
+            return 0;
+        }
+        
+        if(x===0){
+            return 1;
+        }
+
+        if(x===2){
+            return 2;
+        }
+
+        if(x===3){
+            return 6;
+        }
+
+        if(x===4){
+            return 24;
+        }
+
+        if(x===5){
+            return 120;
+        }
+
+        if(x>15){
+            return 0;
+        }
+
         return 0;
     }
-    
-    if(x===0){
-        return 1;
-    }
-
-    if(x===2){
-        return 2;
-    }
-
-    if(x===3){
-        return 6;
-    }
-
-    if(x===4){
-        return 24;
-    }
-
-    if(x===5){
-        return 120;
-    }
-
-    if(x>15){
-        return 0;
-    }
-
-    return 0;
-}
