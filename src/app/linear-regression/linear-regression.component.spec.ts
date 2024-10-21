8e5cb0dc76e8c3a56f982578d5f718132b18e205
MiguelLaraB <159199307+MiguelLaraB@users.noreply.github.com>
2024-10-21 14:53:01 -0600

    create regression (#13)

---
+++ src/app/linear-regression/linear-regression.component.spec.ts
@@ -0,0 +1,89 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LinearRegressionComponent } from './linear-regression.component';
+import { LinearRegression } from '../classes/linear_regression';
+
+describe('LinearRegressionComponent', () => {
+  let component: LinearRegressionComponent;
+  let fixture: ComponentFixture<LinearRegressionComponent>;
+  let linearRegression: LinearRegressionComponent;
+
+  beforeEach(() => {
+    linearRegression = new LinearRegressionComponent();
+  });
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LinearRegressionComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LinearRegressionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+
+  it('Should return b0 approximately -22.55 with the dataset Data_Test1', async () => {
+    const result = linearRegression.getB0([130, 650, 99, 150, 128, 302, 95, 945, 368, 961], [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]);
+    expect(result).toBeCloseTo(-22.55, 2);
+  });
+
+  it('Should return b1 approximately 1.7279 with the dataset Data_Test1', async () => {
+    const result = linearRegression.getB1([130, 650, 99, 150, 128, 302, 95, 945, 368, 961], [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]);
+    expect(result).toBeCloseTo(1.7279, 2);
+  });
+
+  it('Should return yk approximately 644.429 with the dataset Data_Test1 if x=386', async () => {
+    const result = linearRegression.getYK([130, 650, 99, 150, 128, 302, 95, 945, 368, 961], [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601], 386);
+    expect(result).toBeCloseTo(644.429, 2);
+  });
+
+  it('Should return b0 approximately -4.604 with the dataset Data_Test4', async () => {
+    const result = linearRegression.getB0([130, 650, 99, 150, 128, 302, 95, 945, 368, 961], [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]);
+    expect(result).toBeCloseTo(-4.039, 2);
+  });
+
+  it('Should return b1 approximately 0.16064 with the dataset Data_Test4', async () => {
+    const result = linearRegression.getB1([130, 650, 99, 150, 128, 302, 95, 945, 368, 961], [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]);
+    expect(result).toBeCloseTo(0.1681, 2);
+  });
+
+  it('Should return yk approximately 60.858 with the dataset Data_Test4 if x=386', async () => {
+    const result = linearRegression.getYK([130, 650, 99, 150, 128, 302, 95, 945, 368, 961], [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2], 386);
+    expect(result).toBeCloseTo(60.858, 2);
+  });
+
+  it('Should return b0 approximately -23.92 with the dataset Data_Test3', async () => {
+    const result = linearRegression.getB0([163, 765, 141, 166, 137, 355, 136, 1206, 433, 1130], [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]);
+    expect(result).toBeCloseTo(-23.92, 2);
+  });
+
+  it('Should return b1 approximately 1.43097 with the dataset Data_Test3', async () => {
+    const result = linearRegression.getB1([163, 765, 141, 166, 137, 355, 136, 1206, 433, 1130], [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]);
+    expect(result).toBeCloseTo(1.43097, 2);
+  });
+
+  it('Should return yk approximately 528.4294 with the dataset Data_Test3 if x=386', async () => {
+    const result = linearRegression.getYK([163, 765, 141, 166, 137, 355, 136, 1206, 433, 1130], [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601], 386);
+    expect(result).toBeCloseTo(528.4294, 2);
+  });
+
+  it('Should return b0 approximately -4.604 with the dataset Data_Test4', async () => {
+    const result = linearRegression.getB0([163, 765, 141, 166, 137, 355, 136, 1206, 433, 1130], [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]);
+    expect(result).toBeCloseTo(-4.604, 2);
+  });
+
+  it('Should return b1 approximately 0.140164 with the dataset Data_Test4', async () => {
+    const result = linearRegression.getB1([163, 765, 141, 166, 137, 355, 136, 1206, 433, 1130], [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]);
+    expect(result).toBeCloseTo(0.140164, 2);
+  });
+
+  it('Should return yk approximately 60.858 with the dataset Data_Test4 if x=386', async () => {
+    const result = linearRegression.getYK([163, 765, 141, 166, 137, 355, 136, 1206, 433, 1130], [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2], 386);
+    expect(result).toBeCloseTo(49.4994, 2);
+  });
+});
