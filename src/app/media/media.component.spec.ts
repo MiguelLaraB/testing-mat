108ec231357e1dfed030f53f7a145b3b1d05731b
MiguelLaraB <zS22004395@estudiantes.uv.mx>
2024-10-21 16:03:19 -0600

    dev

--- src/app/media/media.component.spec.ts
+++ src/app/media/media.component.spec.ts
@@ -21,12 +21,12 @@ describe('MediaComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Should return media = 550.6 with test 1', () => {
+  it('Should return media = 550.6 with array [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503]', () => {
     const result = media.getMedia([160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503]);
     expect(result).toBe(550.6);
   });
 
-  it('Should return media = 60.32 with test 2', () => {
+  it('Should return media = 60.32 with array [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]', () => {
     const result = media.getMedia([15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]);
     expect(result).toBe(60.32);
   });
--- src/app/stddev/stddev.component.spec.ts
+++ src/app/stddev/stddev.component.spec.ts
@@ -1,24 +1,37 @@
-import { Component } from '@angular/core';
-import { Stddev } from '../classes/stddev';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { StddevComponent } from './stddev.component';
 
-@Component({
-  selector: 'app-stddev',
-  standalone: true,
-  imports: [],
-  templateUrl: './stddev.component.html',
-  styleUrl: './stddev.component.css'
-})
+describe('StddevComponent', () => {
+  let component: StddevComponent;
+  let fixture: ComponentFixture<StddevComponent>;
+  let stddev: StddevComponent;
 
-export class StddevComponent {
+  beforeEach(() => {
+    stddev = new StddevComponent();
+  });
 
-  stddev: Stddev;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [StddevComponent]
+    })
+    .compileComponents();
 
-  constructor() {
-    this.stddev = new Stddev();
-  }
+    fixture = TestBed.createComponent(StddevComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-  getStddev(array: number[]): number {
-    console.log(this.stddev.calcularStddev(array));
-    return this.stddev.calcularStddev(array);
-  }
-}
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Should return stddev = 572.03 with array [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503]', () => {
+    const result = stddev.getStddev([160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503]);
+    expect(result).toBe(572.03);
+  });
+
+  it('Should return stddev = 62.26 with array [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]', () => {
+    const result = stddev.getStddev([15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]);
+    expect(result).toBe(62.26);
+  });
+});
