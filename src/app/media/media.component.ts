14b22025d696d1531795d121897381ee869d8a4f
MiguelLaraB <zS22004395@estudiantes.uv.mx>
2024-10-21 13:38:40 -0600

    media upd

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -1,27 +1,22 @@
 import { Component } from '@angular/core';
+import { Media } from '../classes/media';
 
 @Component({
   selector: 'app-media',
   standalone: true,
   imports: [],
   templateUrl: './media.component.html',
-  styleUrls: ['./media.component.css']
+  styleUrl: './media.component.css'
 })
 export class MediaComponent {
 
-  column1 = [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503];
-  column2 = [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2];
+  media : Media;
 
-  calculateMean(values: number[]): number {
-    const sum = values.reduce((a, b) => a + b, 0);
-    return sum / values.length;
+  constructor() {
+    this.media = new Media();
   }
 
-  displayResults() {
-    const mean1 = this.calculateMean(this.column1).toFixed(2); // 550.60
-    const mean2 = this.calculateMean(this.column2).toFixed(2); // 60.32
-    
-    console.log(`Mean for column 1: ${mean1}`);
-    console.log(`Mean for column 2: ${mean2}`);
-  }  
+  getMedia(array: number[]): number {
+    return this.media.calcularMedia(array);
+  }
 }
