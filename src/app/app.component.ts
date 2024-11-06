bb6f746812a37ada611645c2562b5f10c790b0dc
MiguelLaraB <zS22004395@estudiantes.uv.mx>
2024-11-06 00:42:25 -0600

    media finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { CorrelationComponent } from './correlation/correlation.component';
 import { LinearRegressionComponent } from './linear-regression/linear-regression.component';
 import { RouterLink } from '@angular/router';
+import { MediaComponent } from './media/media.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet,CommonModule, FormsModule, CorrelationComponent, LinearRegressionComponent],
+  imports: [RouterLink, RouterOutlet,CommonModule, FormsModule, CorrelationComponent, LinearRegressionComponent
+    , MediaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
