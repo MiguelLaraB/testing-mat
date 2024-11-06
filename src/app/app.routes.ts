a40c48d8af53d45987fcbeaac22c656b54057a3b
MiguelLaraB <159199307+MiguelLaraB@users.noreply.github.com>
2024-11-06 01:04:10 -0600

    Stddev (#38)
    * stddev act
    
    * stddev finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MediaComponent } from './media/media.component';
 import { CorrelationComponent } from './correlation/correlation.component';
 import { LinearRegressionComponent } from './linear-regression/linear-regression.component';
+import { StddevComponent } from './stddev/stddev.component';
 
 export const routes: Routes = [
     { path: 'media', component: MediaComponent },
     { path: 'correlation', component: CorrelationComponent},
     { path: 'linear-regression', component: LinearRegressionComponent},
+    { path: 'stddev', component: StddevComponent},
 ];
